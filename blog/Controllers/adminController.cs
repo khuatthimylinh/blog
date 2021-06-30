7b518911320355d369e0fecfd1cc328dec61dd75
khuatthimylinh <ktml20112000@gmail.com>
2021-06-30 20:26:09 +0700

    first commit

---
+++ blog/Controllers/adminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: admin
+        public ActionResult Statis()
+        {
+            return View();
+        }
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult post()
+        {
+            return View();
+        }
+    }
+}
